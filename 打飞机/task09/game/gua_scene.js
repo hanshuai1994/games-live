9b499b60acf15b9e9b3e02a018fe860e9a91a165
Hanshuai1994 <709759847@qq.com>
2018-01-13 22:11:14 +0800

    task09

---
+++ 打飞机/task09/game/gua_scene.js
@@ -0,0 +1,27 @@
+class GuaScene {
+    constructor(game) {
+        this.game = game
+        this.elements = []
+    }
+    static new(game) {
+        var i = new this(game)
+        return i
+    }
+    addElement(img) {
+        img.scene = this
+        this.elements.push(img)
+    }
+    draw() {
+        for (var i = 0; i < this.elements.length; i++) {
+            var e = this.elements[i]
+            // this.game.drawImage(e)
+            e.draw()
+        }
+    }
+    update() {
+        for (var i = 0; i < this.elements.length; i++) {
+            var e = this.elements[i]
+            e.update()
+        }
+    }
+}
